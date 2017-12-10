1ee4a0649c9fc869c22c52216d6f74cac8437fe8
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2017-12-10 18:47:22 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,15 +5,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "home | いとう( ˙-˙ )WEB開発中"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "よくある質問 | いとう( ˙-˙ )WEB開発中"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "WEBエンジニアとは | いとう( ˙-˙ )WEB開発中"
   end
 end
