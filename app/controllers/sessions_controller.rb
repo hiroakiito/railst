0175b9e5d549aa423a74dd2316accef37c1d0095
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-01-21 00:13:09 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,7 @@ class SessionsController < ApplicationController
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
-      redirect_to user
+      redirect_back_or user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'パスワード/メールアドレスが間違っています'
