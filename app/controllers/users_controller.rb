8884af2c2b32f2cd0d589a2e52140c0476fefb30
Hiroaki Ito <pigaru.gooood.mar19@gmail.com>
2018-01-28 06:32:24 +0000

    10.33

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,8 @@ class UsersController < ApplicationController
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
-        flash[:danger] = "ログインしてください"
+        store_location
+        flash[:danger] = "Please log in."
         redirect_to login_url
       end
     end
